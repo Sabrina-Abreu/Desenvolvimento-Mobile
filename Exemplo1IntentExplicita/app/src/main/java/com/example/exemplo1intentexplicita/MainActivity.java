91b4235f883a6c0786674555b3bff2de8b1f800e
Sabrina Cardoso de Abreu <sabrina.cardoso@dinfor.uniceub>
2026-09-02 10:00:06 -0300

    Atualizando imagens de fundo e adicionando outras duas Activitys

--- Exemplo1IntentExplicita/app/src/main/java/com/example/exemplo1intentexplicita/MainActivity.java
+++ Exemplo1IntentExplicita/app/src/main/java/com/example/exemplo1intentexplicita/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.exemplo1intentexplicita;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAbrir2Activity,btnAbrir3Activity, btnAbrir4Activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnAbrir2Activity = (Button) findViewById(R.id.cmdAbrir2Activity);
+        btnAbrir3Activity = (Button) findViewById(R.id.cmdAbrir3Activity);
+        btnAbrir4Activity = (Button) findViewById(R.id.cmdAbrir4Activity);
+
+        btnAbrir2Activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
         });
+
+
+
     }
 }
